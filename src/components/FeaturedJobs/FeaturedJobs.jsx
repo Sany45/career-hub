0850e5a5ee005c48dff5f16d62aaa040cad15d34
sany45 <sanybhuiyan97@gmail.com>
2024-03-23 17:58:23 +0600

    add-featured-data-but-not-complete

--- src/components/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -1,11 +1,26 @@
+import { useEffect, useState } from "react";
 
 
 const FeaturedJobs = () => {
+    const [jobs, setJobs] = useState([]);
+
+    useEffect(() => {
+        fetch('jobs.json')
+            .then(res => res.json())
+            .then(data => setJobs(data));
+    }, [])
+
+
     return (
         <div>
             <div className="text-center">
-                <h2 className="text-5xl">Featured Jobs</h2>
+                <h2 className="text-5xl">Featured Jobs: {jobs.length}</h2>
                 <p>Explore thousands of job opportunities with all the information you need. Its your future</p>
+            </div>
+            <div>
+            {
+                jobs.map(job => <Job key={job.id} job={job}></Job>)
+            }
             </div>
         </div>
     );
